4208383c648ff1b3aee0015969179580fa6e5cec
seb <seb@kali>
2021-12-17 09:23:27 +0000

    Initial import

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func coucou(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "coucou\n")
+	fmt.Fprint(w, req.Method+"\n")
+}
+
+func uploadGet(w http.ResponseWriter) {
+	body := `
+		<html>
+			<body>
+				<p>Upload file</p>
+				<form action="/upload" method="post" enctype="multipart/form-data">
+					<input type="file" name="file"/>
+					<input type="Submit" value="Upload" name="submit"/>
+				</form>
+			</body>
+		</html>
+	`
+	fmt.Fprintln(w, body)
+}
+
+func uploadPost(req *http.Request) {
+	file, handler, err := req.FormFile("file")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0444)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
+}
+
+func uploadHanlder(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		//fmt.Fprintln(w, "GET upload")
+		uploadGet(w)
+	case http.MethodPost:
+		uploadPost(req)
+		fmt.Fprintln(w, "POST upload")
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+func main() {
+	fmt.Println("Starting web server....")
+	http.HandleFunc("/coucou", coucou)
+	http.HandleFunc("/upload", uploadHanlder)
+	http.Handle("/", http.FileServer(http.Dir("/home/seb")))
+	http.ListenAndServe(":8090", nil)
+}
